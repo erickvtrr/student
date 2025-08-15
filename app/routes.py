4a6e6e274ed6572c3a7ca2c1ffb68882c1425fbb
erickvtrr <143184235+erickvtrr@users.noreply.github.com>
2025-08-15 13:49:43 +0000

    criação página login

--- app/routes.py
+++ app/routes.py
@@ -7,3 +7,18 @@ main_bp = Blueprint('main', __name__)
 @main_bp.route('/index')
 def index():
     return render_template('index.html')
+
+
+@main_bp.route('./login', methods=['GET', 'POST'])
+def login():
+
+    if request.method == 'GET':
+        return render_template('login.html')
+
+
+username_digitado = request.form.get('username')
+flash(f'Tentativa de login com o usuário: {username_digitado}')
+
+
+return redirect(url_for('main.login'))
+# Handle login logic here
